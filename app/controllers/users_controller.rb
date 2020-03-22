82b7855945d2fc4d81efc10cde3cb65199f3629f
nobu.azu <jonsonjonson1210@gmail.com>
2020-03-22 19:46:05 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to '/'
     else
       flash.now[:danger] = 'ユーザ登録に失敗しました。'
       render :new
